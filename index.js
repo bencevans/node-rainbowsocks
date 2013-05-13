f66114f2b62cfa21b5ce7526c655d8ab48b9e0ec
Ben Evans <ben@bensbit.co.uk>
2013-05-13 23:41:29 +0100

    tidy

--- index.js
+++ index.js
@@ -78,9 +78,8 @@ RainbowSocks.prototype.request = function(cmdBuf, domain, port, callback) {
 
   this.socket.once('data', function(data) {
     if(data[0] !== 0) return callback(new Error('response missing null byte'));
-    if(data[1] == 0x5a) {
-      callback(null, _this.socket);
-    } else if(data[1] == 0x5b) return callback(new Error('request rejected or failed'));
+    if(data[1] == 0x5a) return callback(null, _this.socket);
+    else if(data[1] == 0x5b) return callback(new Error('request rejected or failed'));
     else if(data[1] == 0x5c) return callback(new Error('request failed because client is not running identd (or not reachable from the server)'));
     else if(data[1] == 0x5d) return callback(new Error('request failed because client\'s identd could not confirm the user ID string in the request'));
     else return callback(new Error('unknown response status'));
